862afdc6ed5102d5ace4e3d5364e8bff81058053
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2024-07-07 00:33:11 +0600

    ..
    ui updated

--- MinimalDesk/AddView.swift
+++ MinimalDesk/AddView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import TabBarModule
-let screenWidth = UIScreen.main.bounds.width
+ 
 
 
 
--- MinimalDesk/Widget.swift
+++ MinimalDesk/Widget.swift
@@ -1,42 +1,93 @@
-
 //
-//  CategoryListView.swift
-//  OutletExpense
+//  ContentView.swift
+//  MinimalDesk
 //
-//  Created by Mamunur Rahaman on 8/17/23.
+//  Created by Sadiqul Amin on 6/7/24.
 //
 
 import SwiftUI
+import TabBarModule
+let screenWidth = UIScreen.main.bounds.width
+
 
 
 struct Widget: View {
     
-    @Environment(\.colorScheme) var colorScheme
-
+    @State var widthToSet:CGFloat =  0
+    @State var heightToSet:CGFloat =  0
+    @State var gap:CGFloat = 0
     
-   
+    
     var body: some View {
-        ZStack {
+        VStack {
             
             Color.black.edgesIgnoringSafeArea(.all)
-            VStack {
+            Spacer()
+            
+            
+            Spacer() // Pushes the following view to the bottom
+            
+
+            
+            HStack(spacing:gap) {
                 
-            }
+                
+                
+                VStack(spacing: 8) {
+                    Image("widghet") // Replace "yourImageName" with the name of your image asset
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 35, height: 35)
+                         
+                        
+                    
+                    Text("Customize\n Wedget")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                         
+                    
+                   
+                }
+                .frame(width: widthToSet, height: heightToSet)
+                .background(Color(red: 41/255, green: 44/255, blue: 53/255))
+                .cornerRadius(10)
+                
+                VStack(spacing: 8) {
+                     
+                    Image("widghet") // Replace "yourImageName" with the name of your image asset
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 35, height: 35)
+                    
+                    Text("Customize\nTop Wedget")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                         
+                         
+                     
+                }
+                .frame(width: widthToSet, height: heightToSet)
+                .background(Color(red: 41/255, green: 44/255, blue: 53/255))
+                .cornerRadius(10)
+              
+              
+            }.padding(.bottom,40)
+             
            
-        }
-        .edgesIgnoringSafeArea(.all)
-        .background(colorScheme == .dark ? Color.black : Color.black)
+        } .background(Color.black)
+            
         .onAppear {
-             print("it has been called")
+            
+            widthToSet = (screenWidth * 0.85)/2.0
+            gap = (screenWidth - widthToSet * 2)/3.0
+            heightToSet = (112 * widthToSet) / 176.0
+            // Perform any actions you want when the view appears
         }
         
-    }
-    
-}
-
-struct Widget_Previews: PreviewProvider {
-    static var previews: some View {
-        Widget()
+        
     }
 }
 
+#Preview {
+    Widget()
+}
