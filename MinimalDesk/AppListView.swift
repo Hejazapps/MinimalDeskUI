630d03653305916e0bf737800890c3fa5e5b971f
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2024-07-13 15:42:13 +0600

    ..
    UI UPDATED

--- MinimalDesk/AppListView.swift
+++ MinimalDesk/AppListView.swift
@@ -1,59 +1,78 @@
-//
-//  ContentView.swift
-//  MinimalDesk
-//
-//  Created by Sadiqul Amin on 6/7/24.
-//
-
 import SwiftUI
-import TabBarModule
- 
-import Combine
-import Firebase
-
 
 
 struct AppListView: View {
     
     @ObservedObject private var viewModel: FirebaseDataViewModel
-
-    @State var widthToSet:CGFloat =  0
-    @State var heightToSet:CGFloat =  0
-    @State var gap:CGFloat = 0
+    @Environment(\.dismiss) var dismiss
     
+    @State var widthToSet: CGFloat = 0
+    @State var heightToSet: CGFloat = 0
+    @State var gap: CGFloat = 0
+    @State private var searchText = ""
+
     
     init(viewModel: FirebaseDataViewModel) {
         print("[FirebaseDataView] IN")
-        
         self.viewModel = viewModel
         viewModel.fetchAllSubscribers()
     }
-   
     
     var body: some View {
         VStack {
-            
             Color.black.edgesIgnoringSafeArea(.all)
-            Spacer()
+            
+            VStack(spacing: 5) {
+              
+                HStack {
+                    VStack {
+                        Button(action: {
+                            dismiss()
+                        }) {
+                            Image("Back")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                        }
+                    }
+                    .padding(.trailing, 30)
+                    
+                    Spacer()
+                }
+                .background(Color.black)
+                
+                HStack {
+                    Spacer()
+                    Text("Add Favorite Apps")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                .background(Color.black)
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("Pick your top 6 frequently used apps to keep on your home screen for easy access, avoiding clutter and distractions.")
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color(red: 142/255, green: 142/255, blue: 142/255))
+                        
+                     
+                         
+                    Spacer()
+                }
+                .padding()
+                .background(Color.black)
+                
+                
+                TextField("Search", text: $searchText)
+                               .padding()
+                               .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
             
             
-            Spacer() // Pushes the following view to the bottom
-            
-
-           
-           
-        } .background(Color.black)
-            
-        .onAppear {
-            
-             
-           
         }
-        
-        
+        .background(Color.black)
     }
-}
-
-#Preview {
-    AppListView(viewModel: FirebaseDataViewModel())
 }
