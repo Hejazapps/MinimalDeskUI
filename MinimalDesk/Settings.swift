16febb5cb5e7af9c60955974903ea1c1dfc8f213
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2024-07-14 20:12:18 +0600

    ..
    scroll view added

--- MinimalDesk/Settings.swift
+++ MinimalDesk/Settings.swift
@@ -24,49 +24,49 @@ struct Settings: View {
             
             Color.black.edgesIgnoringSafeArea(.all)
             
-            VStack(spacing: 10) {
+            
+            ScrollView {
                 
-                Text("Settings").bold()
-                    .foregroundColor(Color.white).font(.system(size: 40))
-                    .padding(.top,10)
-                
-                Image("topView")
-                    .resizable()
-                    .frame(width: screenWidth * 0.8, height: getHeight())
-                
-                Text("Themes").foregroundColor(Color.white)
-                
-                
-                VStack {
-                    Picker(selection: $favoriteColor, label: Text("What is your favorite color?")) {
-                        ForEach(0..<colors.count) { index in
-                            Text(colors[index])
-                                .tag(index)
+                VStack(spacing: 10) {
+                    
+                    Text("Settings").bold()
+                        .foregroundColor(Color.white).font(.system(size: 40))
+                        .padding(.top,10)
+                    
+                    Image("topView")
+                        .resizable()
+                        .frame(width: screenWidth * 0.8, height: getHeight())
+                    
+                    Text("Themes").foregroundColor(Color.white)
+                    
+                    
+                    VStack {
+                        Picker(selection: $favoriteColor, label: Text("What is your favorite color?")) {
+                            ForEach(0..<colors.count) { index in
+                                Text(colors[index])
+                                    .tag(index)
+                            }
                         }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .foregroundColor(.white) // Default text color
+                        .pickerStyle(SegmentedPickerStyle())
+                        .foregroundColor(.white) // Default text color
+                        
+                        // Custom segmented control style
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color(red: 39.0 / 255, green: 39.0 / 255, blue: 41.0 / 255)) // Selected segment color
+                        )
+                        
+                        .padding(.horizontal)
+                        
+                        .onAppear {
+                            UISegmentedControl.appearance().selectedSegmentTintColor = .white
+                            UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
+                            UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+                        }
+                    }.frame(width: screenWidth * 0.8)
                     
-                    // Custom segmented control style
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .fill(Color(red: 39.0 / 255, green: 39.0 / 255, blue: 41.0 / 255)) // Selected segment color
-                    )
-                     
-                    .padding(.horizontal)
-                    
-                    .onAppear {
-                        UISegmentedControl.appearance().selectedSegmentTintColor = .white
-                        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
-                        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
-                    }
-                }.frame(width: screenWidth * 0.8)
-                
-               
-                
-                
-                
-            }
+                }
+            }.frame(height: screenHeight - 200)
         }
         .edgesIgnoringSafeArea(.all)
         .background(colorScheme == .dark ? Color.black : Color.black)
